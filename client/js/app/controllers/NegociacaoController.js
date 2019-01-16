dd288a332f58b0b6392291a7100e4f355c09f2e1
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-16 19:53:06 -0200

    Aula 01.2 - E se atualizarmos a view quando o modelo for alterado?

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,10 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._listaNegociacoes = new ListaNegociacoes(function(model){
+            this._negociacoesView.update(model);
+        });
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -21,7 +24,6 @@ class NegociacaoController {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -31,7 +33,6 @@ class NegociacaoController {
 
     apaga() {
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,15 @@
 class ListaNegociacoes {
     
-    constructor() {
+    constructor(armadilha) {
         
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
     
     get negociacoes() {
@@ -17,5 +19,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
+        this._armadilha(this);
     }
 }
