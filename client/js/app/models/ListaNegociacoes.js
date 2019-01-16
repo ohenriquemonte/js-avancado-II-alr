0b2b71879f35785115bfc3f3f172b7d84ba20f1d
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-16 20:07:23 -0200

    Aula 01.4 - Arrow function e seu escopo léxico

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,15 @@
 class ListaNegociacoes {
     
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-        // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
     
     get negociacoes() {
@@ -21,7 +19,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-        // this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
 }
