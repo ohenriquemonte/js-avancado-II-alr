26006201fc612b6a9190ae7e354ebe043cf0c1ea
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-27 23:54:24 -0200

    Aula 03.x - Correções

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,30 +3,31 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
+         
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes, 
+            new ListaNegociacoes(), 
             new NegociacoesView($('#negociacoesView')), 
             'adiciona', 'esvazia');
-        
+       
         this._mensagem = new Bind(
-            new Mensagem(), 
-            new MensagemView($('#mensagemView')), 
-            'texto');
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
     }
     
     adiciona(event) {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
         this._limpaFormulario();   
     }
     
-    apaga() {  
+    apaga() {
+        
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -1,4 +1,5 @@
 class ProxyFactory {
+
     static create(objeto, props, acao) {
 
         return new Proxy(objeto, {
@@ -10,22 +11,26 @@ class ProxyFactory {
                     return function () {
 
                         console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        return acao(target);
+                        let retorno = Reflect.apply(target[prop], target, arguments);
+                        acao(target);
+                        return retorno;
                     }
                 }
 
                 return Reflect.get(target, prop, receiver);
             },
+
             set(target, prop, value, receiver) {
-                if(props.includes(prop)) {
-                    acao(target);
-                }
-                return Reflect.set(target, prop, value, receiver);                
+
+                let retorno = Reflect.set(target, prop, value, receiver);
+                if (props.includes(prop)) acao(target);
+                return retorno;
             }
         });
     }
+
     static _ehFuncao(func) {
+
         return typeof (func) == typeof (Function);
     }
 }
