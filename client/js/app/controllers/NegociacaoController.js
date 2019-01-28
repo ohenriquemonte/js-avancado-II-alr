dce52d5e0a889f9e35a9d278b564cc3228255ec0
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-27 22:41:58 -0200

    Aula 03.1 - Padrão de Projeto Factory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,32 +6,24 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
         
-        let self = this;
-        
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            
-            get(target, prop, receiver) {
-                
-                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-                    
-                    return function() {
-                        
-                         console.log(`interceptando ${prop}`);
-                         Reflect.apply(target[prop], target, arguments);
-                         self._negociacoesView.update(target);
-                    }
-                }
-                
-                return Reflect.get(target, prop, receiver);
-            }
-            
-        });
+        this._listaNegociacoes = ProxyFactory.create(
+                                    new ListaNegociacoes(), 
+                                    ['adiciona', 'esvazia'],
+                                    (model) => 
+                                        this._negociacoesView.update(model)
+                                );
          
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(
+                            new Mensagem(),
+                            ['texto'],
+                            (model) => 
+                                this._mensagemView.update(model)
+                        );
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);    
     }
@@ -42,7 +34,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         
         this._limpaFormulario();   
     }
@@ -50,9 +41,7 @@ class NegociacaoController {
     apaga() {
         
         this._listaNegociacoes.esvazia();
-         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
     
     _criaNegociacao() {
