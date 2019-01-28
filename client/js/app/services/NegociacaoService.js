8566483973792d0e29daeb31c1095acebfbcfd4d
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-28 01:00:27 -0200

    Aula 05.1 - O problema da vida assíncrona

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -27,4 +27,60 @@ class NegociacaoService {
 
         xhr.send();
     }
+
+    obterNegociacoesDaSemanaRetrasada(cb) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/retrasada');
+
+        xhr.onreadystatechange = () => {
+            /* 
+            estados da requisição:
+            1: conexao com o servidor estabelecida
+            2: requisicao recebida
+            3: processando requisicao
+            4: requisicao concluida e com a resposta pronta 
+            */
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto =>
+                            new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as negociações', null);
+                }
+            }
+        };
+
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaAnterior(cb) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/anterior');
+
+        xhr.onreadystatechange = () => {
+            /* 
+            estados da requisição:
+            1: conexao com o servidor estabelecida
+            2: requisicao recebida
+            3: processando requisicao
+            4: requisicao concluida e com a resposta pronta 
+            */
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto =>
+                            new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as negociações', null);
+                }
+            }
+        };
+
+        xhr.send();
+    }
 }
