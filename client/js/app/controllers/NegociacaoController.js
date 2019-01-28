03b399fa7cd36caa1e3dd9e3a7310a7bb419858f
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-28 00:37:50 -0200

    Aula 04.3 - Realizando o parse da resposta

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -29,7 +29,7 @@ class NegociacaoController {
     importaNegociacoes() {
         let xhr = new XMLHttpRequest();
 
-        xhr.open('GET', 'negociacoes/semana');
+        xhr.open('GET', 'negociacoesss/semana');
         
         xhr.onreadystatechange = () => {
             /* 
@@ -41,9 +41,16 @@ class NegociacaoController {
             */
            if(xhr.readyState == 4) {
                 if(xhr.status == 200) {
-                    console.log('Obtendo as negociações do servidor.');
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => 
+                        new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                    .forEach(negociacao => 
+                        this._listaNegociacoes.adiciona(negociacao)
+                    );
+                    this._mensagem.texto = 'Negociações importadas com sucesso!';
                 } else {
-                    console.log('Não foi possível obter as negociações do servidor!');
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana';
                 }
            }
         };
