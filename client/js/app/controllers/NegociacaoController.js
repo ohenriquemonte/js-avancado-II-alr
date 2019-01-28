e6da73ca2368ee6867958573e55fc9659c9d0cd8
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-01-27 23:48:50 -0200

    Aula 03.4 - Parâmetros REST

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,27 +6,23 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes, 
-            this._negociacoesView, 
-            ['adiciona', 'esvazia']);
-        
-        this._mensagemView = new MensagemView($('#mensagemView'));
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
         
         this._mensagem = new Bind(
             new Mensagem(), 
-            this._mensagemView, 
-            ['texto']);
+            new MensagemView($('#mensagemView')), 
+            'texto');
     }
     
     adiciona(event) {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';        
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._limpaFormulario();   
     }
     
